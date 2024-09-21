67e326addca4d207e23d342712a5b1617b521b17
Time Bury <1061472626@qq.com>
2024-09-22 00:56:57 +0800

    【单例】继承Mono的单例模式基类代码格式修改

--- Assets/TBFramework/Scripts/Base/MonoSingleton.cs
+++ Assets/TBFramework/Scripts/Base/MonoSingleton.cs
@@ -2,31 +2,38 @@ using UnityEngine;
 
 namespace TBFramework
 {
-    public class MonoSingleton<T> : MonoBehaviour where T:MonoSingleton<T>
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
     {
         private static T instance;
-        public static T Instance{
-            get{
-                if(instance==null){
-                    instance=new GameObject(typeof(T).Name,typeof(T)).GetComponent<T>();
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameObject(typeof(T).Name, typeof(T)).GetComponent<T>();
                     DontDestroyOnLoad(instance.gameObject);
                 }
                 return instance;
             }
         }
 
-        protected virtual void Awake() {
-            if(instance!=null){
+        protected virtual void Awake()
+        {
+            if (instance != null)
+            {
                 Destroy(this);
                 return;
             }
-            instance=this as T;
+            instance = this as T;
             DontDestroyOnLoad(instance.gameObject);
         }
 
-        protected virtual void OnDestroy() {
-            if(instance==this){
-                instance=null;
+        protected virtual void OnDestroy()
+        {
+            if (instance == this)
+            {
+                instance = null;
             }
         }
     }
