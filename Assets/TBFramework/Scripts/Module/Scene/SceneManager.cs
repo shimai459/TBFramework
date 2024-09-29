3637f44c5d2378de526ed2b68997d47580946f44
Time Bury <1061472626@qq.com>
2024-09-29 17:48:20 +0800

    【场景加载】添加场景加载中的事件

--- Assets/TBFramework/Scripts/Module/Scene/SceneManager.cs
+++ Assets/TBFramework/Scripts/Module/Scene/SceneManager.cs
@@ -14,80 +14,106 @@ namespace TBFramework.Scene
         /// </summary>
         /// <param name="sceneName">场景名</param>
         /// <param name="action">加载完场景后执行的逻辑</param>
-        public void LoadScene(string sceneName,Action action=null,LoadSceneMode loadSceneMode=LoadSceneMode.Single){
+        public void LoadScene(string sceneName, Action action = null, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, loadSceneMode);
-            if(action!=null){
+            if (action != null)
+            {
                 action.Invoke();
             }
         }
 
-        public void LoadScene(int sceneIndex,Action action=null,LoadSceneMode loadSceneMode=LoadSceneMode.Single){
+        public void LoadScene(int sceneIndex, Action action = null, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex, loadSceneMode);
-            if(action!=null){
+            if (action != null)
+            {
                 action.Invoke();
             }
         }
 
-        public void LoadScene(string abName,string sceneName,Action action=null,LoadSceneMode loadSceneMode=LoadSceneMode.Single){
+        public void LoadScene(string abName, string sceneName, Action action = null, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
             ABManager.Instance.LoadAB(abName);
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, loadSceneMode);
-            if(action!=null){
+            if (action != null)
+            {
                 action.Invoke();
             }
         }
 
-        public void LoadScene(string pathURL,string mainName,string abName,string sceneName,Action action=null,LoadSceneMode loadSceneMode=LoadSceneMode.Single){
-            ABManager.Instance.LoadAB(abName,pathURL,mainName);
+        public void LoadScene(string pathURL, string mainName, string abName, string sceneName, Action action = null, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
+            ABManager.Instance.LoadAB(abName, pathURL, mainName);
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, loadSceneMode);
-            if(action!=null){
+            if (action != null)
+            {
                 action.Invoke();
             }
         }
 
-        public void LoadSceneAsync(string sceneName,Action action=null,LoadSceneMode loadSceneMode=LoadSceneMode.Single){
-            MonoManager.Instance.StartCoroutine(ReallyLoadSceneAsync(sceneName,action,loadSceneMode));
+        public void LoadSceneAsync(string sceneName, Action action = null, Action<AsyncOperation> loading = null, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
+            MonoManager.Instance.StartCoroutine(ReallyLoadSceneAsync(sceneName, action, loading, loadSceneMode));
         }
 
-        private IEnumerator ReallyLoadSceneAsync(string sceneName,Action action,LoadSceneMode loadSceneMode){
+        private IEnumerator ReallyLoadSceneAsync(string sceneName, Action action, Action<AsyncOperation> loading, LoadSceneMode loadSceneMode)
+        {
             AsyncOperation ao = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName, loadSceneMode);
-        yield return ao;
-        if(action!=null){
-            action.Invoke();
-        }
-        }
-
-        public void LoadSceneAsync(int sceneIndex,Action action=null,LoadSceneMode loadSceneMode=LoadSceneMode.Single){
-            MonoManager.Instance.StartCoroutine(ReallyLoadSceneAsync(sceneIndex,action,loadSceneMode));
+            while (!ao.isDone)
+            {
+                loading?.Invoke(ao);
+                yield return null;
+            }
+            action?.Invoke();
         }
 
-        private IEnumerator ReallyLoadSceneAsync(int sceneIndex,Action action,LoadSceneMode loadSceneMode){
+        public void LoadSceneAsync(int sceneIndex, Action action = null, Action<AsyncOperation> loading = null, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
+            MonoManager.Instance.StartCoroutine(ReallyLoadSceneAsync(sceneIndex, action, loading, loadSceneMode));
+        }
+
+        private IEnumerator ReallyLoadSceneAsync(int sceneIndex, Action action, Action<AsyncOperation> loading, LoadSceneMode loadSceneMode)
+        {
             AsyncOperation ao = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneIndex, loadSceneMode);
-            yield return ao;
-            if(action!=null){
-                action.Invoke();
+            while (!ao.isDone)
+            {
+                loading?.Invoke(ao);
+                yield return null;
             }
+            action?.Invoke();
         }
 
-        public void LoadSceneAsync(string abName,string sceneName,bool isAsync=false,Action action=null,LoadSceneMode loadSceneMode=LoadSceneMode.Single){
-            if(isAsync){
-                ABManager.Instance.LoadABAsync(abName,(ab)=>{
-                    LoadSceneAsync(sceneName,action,loadSceneMode);
+        public void LoadSceneAsync(string abName, string sceneName, bool isAsync = false, Action action = null, Action<AsyncOperation> loading = null, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
+            if (isAsync)
+            {
+                ABManager.Instance.LoadABAsync(abName, (ab) =>
+                {
+                    LoadSceneAsync(sceneName, action, loading, loadSceneMode);
                 });
-            }else{
-                ABManager.Instance.LoadAB(abName);
-                LoadSceneAsync(sceneName,action,loadSceneMode);
             }
-            
+            else
+            {
+                ABManager.Instance.LoadAB(abName);
+                LoadSceneAsync(sceneName, action, loading, loadSceneMode);
+            }
+
         }
 
-        public void LoadSceneAsync(string abName,string pathURL,string mainName,string sceneName,bool isAsync=false,Action action=null,LoadSceneMode loadSceneMode=LoadSceneMode.Single){
-            if(isAsync){
-                ABManager.Instance.LoadABAsync(abName,pathURL,mainName,(ab)=>{
-                LoadSceneAsync(sceneName,action,loadSceneMode);
-            });
-            }else{
-                ABManager.Instance.LoadAB(abName,pathURL,mainName);
-                LoadSceneAsync(sceneName,action,loadSceneMode);
+        public void LoadSceneAsync(string abName, string pathURL, string mainName, string sceneName, bool isAsync = false, Action action = null, Action<AsyncOperation> loading = null, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        {
+            if (isAsync)
+            {
+                ABManager.Instance.LoadABAsync(abName, pathURL, mainName, (ab) =>
+                {
+                    LoadSceneAsync(sceneName, action, loading, loadSceneMode);
+                });
+            }
+            else
+            {
+                ABManager.Instance.LoadAB(abName, pathURL, mainName);
+                LoadSceneAsync(sceneName, action, loading, loadSceneMode);
             }
         }
 
